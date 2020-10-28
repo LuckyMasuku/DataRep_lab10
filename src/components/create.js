58d37aa2c9641286fd4581b50468e6135abe7c95
Lucky Masuku <g00363602@gmit.ie>
2020-10-28 14:31:36 +0000

    lab complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,13 +1,85 @@
 import React from 'react'
 
-export class Create extends React.Component{
+export class Create extends React.Component {
+//
+    constructor() {
+        super();
+//when the movie is added its also added on the alert
+// the bind helps to pass to the target
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeYear = this.onChangeYear.bind(this);
+        this.onChangePoster = this.onChangePoster.bind(this);
+        this.state = {
+            Title: '',
+            Year: '',
+            Poster: '',
 
-    render(){
-           return(
-        <div>
+        }
+    }
+    //called when the input value changes
+    onChangeTitle(e) {
+        this.setState({
+            Title: e.target.value
+        });
+    }
+    //called when the input value changes
+    //update the state 
+    onChangeYear(e){
+        this.setState({
+            Year: e.target.value
+        });
 
-            <h1>This is my Create Component</h1>
-        </div>
-           );
+    }
+    //called when the input value changes
+    onChangePoster(e){
+        this.setState({
+            Poster: e.target.value
+        });
+    }
+    //when submit is clicked i
+    onSubmit(e) {
+        e.preventDefault();
+        alert("Movie: " + this.state.Title + " " +this.state.Year + " " +this.state.Poster);
+        //form to add data 
+    }
+    render() {
+        return (
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Add Movie Title: </label>
+                        <input type='text'
+                            className='form-control'
+                            value={this.state.Title}
+                            onChange={this.onChangeTitle}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Add Movie Year</label>
+                        <input type='text'
+                        className='form-control'
+                        value={this.state.Year}
+                        onChange={this.onChangeYear}></input>
+                     
+                    </div>
+                    <div className='form-group'>
+                        <label>Movies Poster:</label>
+                        <textarea type='text'
+                        className='form-control'
+                        value={this.state.Poster}
+                        onChange={this.onChangePoster}>
+
+                        </textarea>
+                        </div>
+
+                    <div className="form-group">
+                        <input type='submit'
+                            value='Add Movie'
+                            className='btn btn-primary'></input>
+                    </div>
+
+                </form>
+            </div>
+        );
     }
 } 
--- src/components/read.js
+++ src/components/read.js
@@ -1,47 +1,40 @@
 import React from 'react';
 import { Movies } from './movies';
+import axios from 'axios';
 
 //Class extends from react
 export class Read extends React.Component {
-//render used to run the function
-state = {
-        movies: [
-            {
-                "Title": "Avengers: Infinity War",
-                "Year": "2018",
-                "imdbID": "tt4154756",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            
-            {
-                "Title": "Charlie Wilson's War",
-                "Year": "2007",
-                "imdbID": "tt0472062",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMTgwMDgwMDc4MF5BMl5BanBnXkFtZTYwOTU3MDM4._V1_SX300.jpg"
-            }
-        ]
+    
+    //used to store data in relevant component
+    state = {
+        movies: []
 
-}
+    };
+    //this methord life cycle hooks its called when the component comes active in the view we get an event
+    componentDidMount() {
+//help to get the response 
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+
+            //allows the fullfilled state methord when everything works
+            .then(
+                //function methord
+                (response) => {
+                    this.setState({ movies: response.data.Search })
+                })
+            //if nothing happens or things dont work what do we want on the output
+            .catch(
+                //function methord
+                (error) => {
+                    console.log(error)
+                });
+
+
+    }
+    //render used to run the function
     render() {
-//the return will return and display the command
+        //the return will return and display the command
         return (
-//passing different components
+            //passing different components
             <div>
                 <h1>This is my read Component.</h1>
                 <Movies movies={this.state.movies}></Movies>
